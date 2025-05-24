8990f23f15425466bb8a692a19743311344cacb0
Lowfatm1ilk <shawndaigogo@gmail.com>
2025-05-23 17:26:42 -0700

    scoretrexr

--- Assets/Cannonball/Scripts/DestructableBuilding.cs
+++ Assets/Cannonball/Scripts/DestructableBuilding.cs
@@ -1,11 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 [RequireComponent(typeof(Rigidbody))]
 public class DestructableBuilding : MonoBehaviour
 {
     private Rigidbody rb;
+    private ScoreText scoreTextObject;
+    private TextMeshProUGUI scoreText;
+    int score;
+
+    private void OnAwake()
+    {
+        scoreTextObject = FindObjectOfType<ScoreText>();
+        if(scoreTextObject != null)
+        {
+            scoreText = scoreTextObject.GetComponent<TextMeshProUGUI>();
+            Debug.Log("Found ScoreText");
+        }
+       
+    }
+
+    private void Update()
+    {
+        int.TryParse(scoreText.text, out score);
+    }
 
     void Awake()
     {
@@ -15,5 +34,14 @@ public class DestructableBuilding : MonoBehaviour
     public Rigidbody GetRigidbody()
     {
         return rb;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("ground"))
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
     }
 }
